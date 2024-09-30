9179a672ccce4c56873fd7843b5cd239565d5c8f
djm06294 <jykimjbr860@gmail.com>
2024-09-30 22:18:52 +0900

    다시 config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  // base: mode === "production" ? "/Vite-githubPages-test/" : "/",
-}));
+});
+// export default defineConfig(({ mode }) => ({
+//   plugins: [react()],
+//   base: mode === 'production' ? '/Vite-githubPages-test/' : '/',
+// }));
